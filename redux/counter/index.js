d2e40767e5ae80883a5005dd4a5c2164c8b8165d
Traphume <traphumedev@gmail.com>
2020-08-25 16:34:50 +0700

    Added initialState to counter

--- redux/counter/index.js
+++ redux/counter/index.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const counterSlice = createSlice({
   name: 'counter',
+  initialState: 0,
   reducers: {
     increment: state => state + 1
     decrement: state => state - 1
