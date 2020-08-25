150b921d0b09c26aec45e01fbe27dad902b276e8
Traphume <traphumedev@gmail.com>
2020-08-25 16:48:29 +0700

    Fixed silly mistakes

--- redux/counter/index.js
+++ redux/counter/index.js
@@ -4,7 +4,7 @@ const counterSlice = createSlice({
   name: 'counter',
   initialState: 0,
   reducers: {
-    increment: state => state + 1
+    increment: state => state + 1,
     decrement: state => state - 1
   },
 })
--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { createWrapper } from 'next-redux-wrapper'
 import rootReducer from './rootReducer'
 
-export default configureStore({
+const makeStore = () => configureStore({
   reducer: rootReducer
 })
+
+const wrapper = createWrapper(makeStore, { debug: true })
+
+export default wrapper
