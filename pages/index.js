41ab28695d48a401dfa4dbf74d0f99313c233013
Traphume <traphumedev@gmail.com>
2020-08-25 23:27:01 +0700

    Added simple counter buttons

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from 'next/head'
+import { useSelector, useDispatch } from 'react-redux'
+import { counterActions } from '../redux/counter'
+
+import { styled } from '@material-ui/styles'
 import styles from '../styles/Home.module.css'
+import Button from '@material-ui/core/Button'
+
+const MarginButton = styled(Button)({
+  marginTop: '0.5em'
+})
 
 export default function Home() {
+  const dispatch = useDispatch()
+  const count = useSelector(state => state.counter)
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,9 +22,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Redux Counter with <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{count}</h1>
+        <MarginButton color="primary" variant="contained" onClick={() => dispatch(counterActions.increment())}>INCREASE</MarginButton>
+        <MarginButton color="secondary" variant="outlined" onClick={() => dispatch(counterActions.decrement())}>DECREASE</MarginButton>
       </main>
 
       <footer className={styles.footer}>
@@ -25,6 +37,6 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
-    </div>
+    </div >
   )
 }
