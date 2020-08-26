19938f189b15a827c1770929d46b1aec3e98b69a
Traphume <traphumedev@gmail.com>
2020-08-26 15:03:58 +0700

    Added redux persist to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
-import "../styles/globals.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { useStore } from "react-redux";
 import wrapper from "../redux/store";
 
+import "../styles/globals.css";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const store = useStore((state) => state);
+
+  return (
+    <PersistGate persistor={store.__persistor}>
+      <Component {...pageProps} />;
+    </PersistGate>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
--- redux/store.js
+++ redux/store.js
@@ -1,11 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { createWrapper } from "next-redux-wrapper";
 import rootReducer from "./rootReducer";
 
-const makeStore = () =>
-  configureStore({
-    reducer: rootReducer,
-  });
+const makeStore = ({ isServer }) => {
+  if (isServer) {
+    return configureStore({
+      reducer: rootReducer,
+    });
+  } else {
+    const persistConfig = {
+      key: "nextjs",
+      storage,
+    };
+
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+    const store = configureStore({
+      reducer: persistedReducer,
+    });
+
+    store.__persistor = persistStore(store);
+    return store;
+  }
+};
 
 const wrapper = createWrapper(makeStore, { debug: true });
 
