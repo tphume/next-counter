b0887d681914fe364c274c181ee2068a70f36bc3
Traphume <traphumedev@gmail.com>
2020-08-30 23:03:49 +0700

    Added manifest file to Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { PersistGate } from "redux-persist/integration/react";
 import { useStore } from "react-redux";
 import wrapper from "../redux/store";
@@ -8,9 +9,17 @@ function MyApp({ Component, pageProps }) {
   const store = useStore((state) => state);
 
   return (
-    <PersistGate persistor={store.__persistor}>
-      <Component {...pageProps} />;
-    </PersistGate>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Next.js Counter</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+      </Head>
+      <PersistGate persistor={store.__persistor}>
+        <Component {...pageProps} />;
+      </PersistGate>
+    </>
   );
 }
 
