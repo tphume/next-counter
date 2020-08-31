1896cae0ca92e48f3ae5a6b00d256d84edb037a7
Traphume <traphumedev@gmail.com>
2020-08-31 14:22:27 +0700

    Separated Head element into _document for purely SSG

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <title>Next.js Counter</title>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
